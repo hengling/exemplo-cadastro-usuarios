c909047517934b4809c60b5641ab4cdb82411711
Adolfo Marangoni Hengling <adolfohengling@gmail.com>
2017-09-25 17:38:34 -0400

    Adicionando a dependencia do ui-router

--- app/app.js
+++ app/app.js
@@ -4,11 +4,10 @@
 angular.module('myApp', [
     'ui.router',
     'myApp.usuarios.listagem',
-    'myApp.usuarios.edicao'])
-    .config(function ($stateProvider) {
+    'myApp.usuarios.edicao'
+]).config(function ($stateProvider) {
 
-    })
-    .constant('constants', {
-        apiUrl: 'https://stafapi.herokuapp.com/users',
-        token: 'thisShallBeSecret'
-    });
+}).constant('constants', {
+    apiUrl: 'https://stafapi.herokuapp.com/users',
+    token: 'thisShouldNotBeRevealed'
+});
